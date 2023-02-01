16b90d8fd6c09b6267c8696f50adc58de2c79000
Tom Braul <tobr772@gmail.com>
2023-02-01 15:01:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+Tom Braul - Software Engineer
+
+I am a software engineer focused on web technologies like javascript
+Laravel and golang. I worked in market research for the last 7 years 
+where I used to have a strong data centric aproach to my solutions.
+
+🐦 Twitter: https://twitter.com/tom_braul
+🤖 GitHub:  https://github.com/tombraul
+(Use ctrl/cmd + click on the links)
+`);
